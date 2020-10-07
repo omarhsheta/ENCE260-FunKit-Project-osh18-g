7203b97ea4178f934378572440f6d3e520e81552
George Alexander Holden <gah83@cs18223kq.canterbury.ac.nz>
2020-10-07 17:46:55 +1300

    Added basic ir test

--- game.c
+++ game.c
@@ -3,6 +3,7 @@
 #include "tinygl.h"
 #include "../fonts/font5x7_1.h"
 #include "navswitch.h"
+#include "ir_uart.h"
 #include "button.h"
 #include "stdbool.h"
 
@@ -19,32 +20,42 @@ void display_character (char character)
     tinygl_text (buffer);
 }
 
+void setup_all(void)
+{
+    system_init();
+    tinygl_init(PACER_RATE);
+    navswitch_init();
+    ir_uart_init();
+
+    tinygl_font_set(&font5x7_1);
+    tinygl_text_speed_set(MESSAGE_RATE);
+    tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
+    pacer_init(PACER_RATE);
+}
+
 int main (void)
 {
-
-    system_init();
-    tinygl_init (PACER_RATE);
-    navswitch_init();
-    tinygl_font_set (&font5x7_1);
-    tinygl_text_speed_set (MESSAGE_RATE);
-    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-
-    tinygl_text("Rock Papper Scissors 2: Electric Boogaloo\0");
-
-
-    pacer_init (PACER_RATE);
+    setup_all();
+    tinygl_text("Rock Paper Scissors 2: Electric Boogaloo\0");
     bool ready = false;
-
     while(!ready)
     {
         pacer_wait();
         tinygl_update();
 
-        navswitch_update ();
+        navswitch_update();
         if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-            ready = true;
+            ir_uart_putc('S');
+        }
+
+        if (ir_uart_read_ready_p()) {
+            if (ir_uart_getc() == 'S') {
+                ready = true;
+                ir_uart_putc('S');
+           }
         }
     }
+
     char options[] = "RPS";
     int counter = 0;
     display_character(options[counter]);
