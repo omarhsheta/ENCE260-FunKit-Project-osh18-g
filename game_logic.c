cbddc39e2ffacce07c1fc9c9d717dce57b5f0e1c
Omar <osh18@uclive.ac.nz>
2020-10-12 19:37:23 +1300

    Fixed unnecessary scrolling for Score and win/lose/tie displays

--- game_logic.c
+++ game_logic.c
@@ -111,21 +111,20 @@ int find_winner(char* player_selection, char* other_selection)
  * */
 void display_winner(int result, int* score)
 {
-    if (result == 2) {
-        display_character('W');
-        *score = *score + 1;
-    } else if (result == 1) {
-        display_character('T');
-    } else {
-        display_character('L');
-    }
-
     int counter = 0;
     while(counter < 1000)
     {
-        pacer_wait();
-        tinygl_update();
-        counter++;
+        if (result == 2) {
+            display_character('W');
+            *score = *score + 1;
+        } else if (result == 1) {
+            display_character('T');
+        } else {
+            display_character('L');
+        }
+            pacer_wait();
+            tinygl_update();
+            counter++;
     }
 }
 
@@ -135,10 +134,10 @@ void display_winner(int result, int* score)
  * */
 void display_score(int* score)
 {
-    display_character('0' + *score);
     int counter = 0;
     while(counter < 1000)
     {
+        display_character('0' + *score);
         pacer_wait();
         tinygl_update();
         counter++;
